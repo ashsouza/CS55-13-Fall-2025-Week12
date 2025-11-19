4767ae6f91d00b0aeeee5cbea121f98e8a4ec052
ashsouza <asouza@bearcubs.santarosa.edu>
2025-11-18 20:36:31 -0800

    [id].js change

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -1,50 +1,48 @@
+// pages/posts/[id].js
+
 import Layout from '../../components/layout';
-import { getAllIds, getData } from '../../lib/posts'; 
+import { getData } from '../../lib/posts';
 import Head from 'next/head';
-import Date from '../../components/date'; 
-import utilStyles from '../../styles/utils.module.css'; 
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
 
+export async function getServerSideProps({ params }) {
+  // Fetch a single post from WordPress dynamically at request time
+  const postData = await getData(params.id);
 
-export async function getStaticProps({ params }) {
- const postData = await getData(params.id);
+  // If the post doesn't exist, return a 404 status
+  if (!postData || !postData.ID) {
+    return {
+      notFound: true,
+    };
+  }
 
- return {
-     props: {
-     postData,
-   },
- };
+  return {
+    props: {
+      postData,
+    },
+  };
 }
 
-// This function determines which paths to pre-render for all blog posts.
-export async function getStaticPaths() {
- // Get the IDs of all available posts.
- const paths = await getAllIds();
- return {
- // Return the list of paths to be pre-rendered.
- paths,
- // `fallback: false` means that any path not returned by `getStaticPaths`
-    // will result in a 404 page.
- fallback: false,
-};
-}
-
-// This is the default component for the blog post page.
-// It receives `postData` as a prop from `getStaticProps`.
 export default function Post({ postData }) {
   return (
- // Wrap the page content with the custom Layout component.
-   <Layout>
-    <Head>
-         <title>{postData.post_title}</title>
+    <Layout>
+      <Head>
+        <title>{postData.post_title}</title>
       </Head>
+
       <article>
-       <h1 className={utilStyles.headingXl}>{postData.post_title}</h1>
+        <h1 className={utilStyles.headingXl}>{postData.post_title}</h1>
 
         <div className={utilStyles.lightText}>
           <Date dateString={postData.post_date} />
         </div>
-        <div className={utilStyles.headingMd} dangerouslySetInnerHTML={{ __html: postData.post_content }} />
+
+        <div
+          className={utilStyles.headingMd}
+          dangerouslySetInnerHTML={{ __html: postData.post_content }}
+        />
       </article>
     </Layout>
-   );
+  );
 }
